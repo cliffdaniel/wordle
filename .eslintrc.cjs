4f82003a55b1d58d5b06b28fe01680643995ae30
Daniel Arenas <jdaniel.arenas@gmail.com>
2023-08-07 14:57:15 -0500

    chore: removiendo react-hooks/exhaustive-deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
